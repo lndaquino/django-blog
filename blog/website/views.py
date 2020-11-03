39d79535dec5fa66eefb15068896f3d4123941be
lucasidr <lucas.aquino@idrconsultoria.com.br>
2020-11-03 09:46:17 -0300

    Reading DB

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def helloBlog(request):
   # return HttpResponse('Blog')
   lista = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi', 'Systemctl']
-  data = {'name': 'Lucas Aquino', 'listaTecnologias' : lista}
+  postsList = Post.objects.all()
+  data = {'name': 'Lucas Aquino', 'listaTecnologias' : lista, 'posts': postsList}
+  
   return render(request, 'index.html', data)
