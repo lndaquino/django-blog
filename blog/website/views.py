18a55c2c792e969e7c165d067ba50a55a11548d8
lucasidr <lucas.aquino@idrconsultoria.com.br>
2020-11-03 09:37:24 -0300

    Working with templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+def helloBlog(request):
+  # return HttpResponse('Blog')
+  lista = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi', 'Systemctl']
+  data = {'name': 'Lucas Aquino', 'listaTecnologias' : lista}
+  return render(request, 'index.html', data)
