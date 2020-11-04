b5ca4f7aa4e2e03a185ce1e7758e6e9d1009ff68
lucasidr <lucas.aquino@idrconsultoria.com.br>
2020-11-04 11:17:23 -0300

    Adding IP and setting Degug=False

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$m6@12!w0g#iiz0hk#+pruknhl+j@5z2vi7cp+j!54xxc3d(y&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '34.123.214.58']
 
 
 # Application definition
